3fd8c85a13f84a57f6a3fd04f871ce5dd73a1448
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-08-31 12:07:02 +1000

    t

--- ccjob.py
+++ ccjob.py
@@ -61,7 +61,7 @@ class CommonCrawlJob(MRJob):
         ccfile = warc.WARCFile(fileobj=(GzipStreamFile(temp)))
         LOG.info('CCFILE: %s', ccfile)
         for _i, record in enumerate(ccfile):
-            LOG.info('CCRECORD: %s', record)
+            LOG.info('CCRECORD: %s', record['Content-Length'])
             for key, value in self.process_record(record):
                 yield key, value
             self.increment_counter('commoncrawl', 'processed_records', 1)
