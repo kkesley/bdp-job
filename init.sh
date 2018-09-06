662b45d499995f474af97c0f355b99c2d9788c21
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-09-06 12:34:54 +1000

    use hadoop

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 pip install -r requirements.txt
 chmod +x run_hadoop.sh
-hadoop fs -mkdir input
-hadoop fs -copyFromLocal input/* input
+sudo -H -u hadoop bash -c 'hadoop fs -mkdir input'
+sudo -H -u hadoop bash -c 'hadoop fs -copyFromLocal input/* input'
