36e6830b7f945293d49cfe0404ec33df9c53bd4f
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-09-06 12:02:42 +1000

    yield target uri

--- word_count.py
+++ word_count.py
@@ -3,9 +3,11 @@ from ccjob import CommonCrawlJob
 
 class WordCount(CommonCrawlJob):
     def process_record(self, record):
+        if record['Content-Type'] != 'text/plain':
+            return
         data = record.payload.read()
         self.increment_counter('commoncrawl', 'processed_pages', 1)
-        yield record['Content-Type'], 1
+        yield record['WARC-Target-URI'], 1
 
 
 if __name__ == '__main__':
