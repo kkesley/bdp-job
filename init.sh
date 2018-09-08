1561438ae4c0d1b41e1c85495502bf1afaa6d729
kkesley <kendrick.kesley.dev@gmail.com>
2018-09-08 16:24:01 +1000

    download from repo

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@ pip install -r requirements.txt
 chmod +x run_hadoop.sh
 aws s3 cp s3://commoncrawl/crawl-data/CC-MAIN-2018-34/wet.paths.gz input/
 gunzip input/wet.paths.gz
+split -l 100 input/wet.paths input/splitted-wet-
 sudo -H -u hadoop bash -c 'hadoop fs -mkdir input'
 sudo -H -u hadoop bash -c 'hadoop fs -copyFromLocal input/* input'
