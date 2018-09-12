b2593c227bbcea135b58874f8b2ce352296ecf47
kkesley <kendrick.kesley.dev@gmail.com>
2018-09-12 19:51:09 +1000

    test

--- pagerank.py
+++ pagerank.py
@@ -10,9 +10,14 @@ class PageRank(CommonCrawlJob):
         data = json.loads(payload)
         if data['Envelope']['WARC-Header-Metadata']['WARC-Type'] != 'response':
             return
-        for link in data['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']:
-            yield link['url'], 1           
-        self.increment_counter('commoncrawl', 'processed_pages', 1)
-
+        
+        try:
+            for link in data['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']:
+                print link
+                yield link['url'], 1     
+            self.increment_counter('commoncrawl', 'processed_server_headers', 1)
+        except KeyError:
+            pass
+        
 if __name__ == '__main__':
     PageRank.run()
