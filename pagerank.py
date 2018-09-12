cec5e525f3a2fa00dff4179ed1ff96060a9ccc73
kkesley <kendrick.kesley.dev@gmail.com>
2018-09-12 19:57:38 +1000

    test

--- pagerank.py
+++ pagerank.py
@@ -4,19 +4,16 @@ import json
 
 class PageRank(CommonCrawlJob):
     def process_record(self, record):
-        print record['Content-Type']
         if record['Content-Type'] != 'application/json':
             return
         payload = record.payload.read()
         data = json.loads(payload)
-        print data
         if data['Envelope']['WARC-Header-Metadata']['WARC-Type'] != 'response':
             return
         
         try:
             for link in data['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']:
-                print link
-                yield link['url'], 1     
+                yield link, 1     
             self.increment_counter('commoncrawl', 'processed_server_headers', 1)
         except KeyError:
             pass
