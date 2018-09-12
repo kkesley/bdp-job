e151e7b02bf3c873b37959cae6e99bda6f73576f
kkesley <kendrick.kesley.dev@gmail.com>
2018-09-12 19:46:49 +1000

    link

--- pagerank.py
+++ pagerank.py
@@ -1,9 +1,17 @@
 from collections import Counter
 from ccjob import CommonCrawlJob
+import json
 
 class PageRank(CommonCrawlJob):
     def process_record(self, record):
-        yield "", 1                        
+        if record['Content-Type'] != 'application/json':
+            return
+        payload = record.payload.read()
+        data = json.loads(payload)
+        if data['Envelope']['WARC-Header-Metadata']['WARC-Type'] != 'response':
+            return
+        for link in data['Envelope']['Payload-Metadata']['HTTP-Response-Metadata']['HTML-Metadata']['Links']:
+            yield link['url'], 1           
         self.increment_counter('commoncrawl', 'processed_pages', 1)
 
 if __name__ == '__main__':
