ed8e402475855de710dd39ecb024428a4c1988fa
kkesley <kendrick.kesley.dev@gmail.com>
2018-09-12 20:25:38 +1000

    a

--- ccjob.py
+++ ccjob.py
@@ -7,6 +7,7 @@ from gzipstream import GzipStreamFile
 from tempfile import TemporaryFile
 from mrjob.job import MRJob
 from mrjob.util import log_to_stream
+import json
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger(__name__)
 class CommonCrawlJob(MRJob):
@@ -58,6 +59,14 @@ class CommonCrawlJob(MRJob):
         ccfile = warc.WARCFile(fileobj=(GzipStreamFile(temp)))
         LOG.info('Attempting MapReduce Job......')
         for _i, record in enumerate(ccfile):
+            if record['Content-Type'] != 'application/json':
+                continue
+            payload = record.payload.read()
+            data = json.loads(payload)
+            LOG.info('%s', data)
+            if data['Envelope']['WARC-Header-Metadata']['WARC-Type'] != 'response':
+                continue
+            
             for key, value in self.process_record(record):
                 yield key, value
             self.increment_counter('commoncrawl', 'processed_records', 1)
