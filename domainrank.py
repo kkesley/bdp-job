b376d777b2227ea52aec611934ba0ef560624eb7
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-09-14 13:25:04 +1000

    a

--- domainrank.py
+++ domainrank.py
@@ -5,6 +5,8 @@ from mrjob.step import MRStep
 import logging
 import json
 import re
+import string
+import math
 regex = re.compile(
     r'^(?:http|ftp)s?://' # http:// or https://
     r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain...
