afe52b522b25185267871e42b9fffdc4cb661bca
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-09-14 11:32:25 +1000

    reducers to 1 for sorting

--- domainrank.py
+++ domainrank.py
@@ -114,7 +114,7 @@ class DomainRank(CommonCrawlJob):
     def steps(self):
         return [MRStep(mapper=self.mapper, combiner=self.combiner, reducer=self.reducer)] + \
         [MRStep(mapper=self.second_mapper, combiner=self.combiner, reducer=self.reducer, jobconf={"mapred.reduce.tasks": 10})] * 1 + \
-        [MRStep(mapper=self.sorting_mapper, reducer=self.sorting_reducer, jobconf={"mapred.reduce.tasks": 5})]
+        [MRStep(mapper=self.sorting_mapper, reducer=self.sorting_reducer, jobconf={"mapred.reduce.tasks": 1})]
         
 
 if __name__ == '__main__':
