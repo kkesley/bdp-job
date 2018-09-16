62238793f7cb8e60f49ba06e6f0b1f762e7f02ab
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-09-17 09:47:21 +1000

    add split save space

--- init.sh
+++ init.sh
@@ -4,8 +4,9 @@ pip install -r requirements.txt
 chmod +x run_hadoop.sh
 aws s3 cp s3://commoncrawl/crawl-data/CC-MAIN-2018-34/wat.paths.gz input/
 gunzip input/wat.paths.gz
-split -l 100 input/wat.paths input/splitted-hundred-wat-
-split -l 1000 input/wat.paths input/splitted-thousand-wat-
 split -l 10000 input/wat.paths input/splitted-tenthousand-wat-
+split -l 1000 input/splitted-tenthousand-wat-aa input/splitted-thousand-wat-
+split -l 100 input/splitted-thousand-wat-aa input/splitted-hundred-wat-
+split -l 10 input/splitted-hundred-wat-aa input/splitted-ten-wat-
 sudo -H -u hadoop bash -c 'hadoop fs -mkdir input'
 sudo -H -u hadoop bash -c 'hadoop fs -copyFromLocal input/* input'
